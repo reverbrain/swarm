e48d81b017f4fb7b430facda47a7eeac78fb6fe2
Ruslan Nigmatullin <euroelessar@yandex.ru>
2013-06-24 20:05:07 +0400

    Fixed indentation in stream.hpp

--- thevoid/stream.hpp
+++ thevoid/stream.hpp
@@ -25,102 +25,108 @@ namespace thevoid {
 class reply_stream
 {
 public:
-    reply_stream();
-    virtual ~reply_stream();
+	reply_stream();
+	virtual ~reply_stream();
 
-    virtual void send_headers(const swarm::network_reply &rep,
-                              const boost::asio::const_buffer &content,
-                              const std::function<void (const boost::system::error_code &err)> &handler) = 0;
-    virtual void send_data(const boost::asio::const_buffer &buffer,
-                            const std::function<void (const boost::system::error_code &err)> &handler) = 0;
-    virtual void close(const boost::system::error_code &err) = 0;
+	virtual void send_headers(const swarm::network_reply &rep,
+				  const boost::asio::const_buffer &content,
+				  const std::function<void (const boost::system::error_code &err)> &handler) = 0;
+	virtual void send_data(const boost::asio::const_buffer &buffer,
+			       const std::function<void (const boost::system::error_code &err)> &handler) = 0;
+	virtual void close(const boost::system::error_code &err) = 0;
 
-    virtual void send_error(swarm::network_reply::status_type type) = 0;
+	virtual void send_error(swarm::network_reply::status_type type) = 0;
 };
 
 class base_request_stream
 {
 public:
-    virtual ~base_request_stream() {}
+	virtual ~base_request_stream() {}
 
-    virtual void on_headers(const swarm::network_request &req) = 0;
-    virtual void on_data(const boost::asio::const_buffer &buffer) = 0;
-    virtual void on_close(const boost::system::error_code &err) = 0;
+	virtual void on_headers(const swarm::network_request &req) = 0;
+	virtual void on_data(const boost::asio::const_buffer &buffer) = 0;
+	virtual void on_close(const boost::system::error_code &err) = 0;
 
-    void initialize(const std::shared_ptr<reply_stream> &reply) { m_reply = reply; }
+	void initialize(const std::shared_ptr<reply_stream> &reply) { m_reply = reply; }
 
 protected:
-    std::shared_ptr<reply_stream> get_reply()
-    {
-        if (__builtin_expect(!m_reply, false))
-            throw std::logic_error("request_stream::m_reply is null");
+	std::shared_ptr<reply_stream> get_reply()
+	{
+		if (__builtin_expect(!m_reply, false))
+			throw std::logic_error("request_stream::m_reply is null");
 
-        return m_reply;
-    }
+		return m_reply;
+	}
 
 private:
-    std::shared_ptr<reply_stream> m_reply;
+	std::shared_ptr<reply_stream> m_reply;
 };
 
 template <typename Server>
 class request_stream : public base_request_stream
 {
 public:
-    request_stream() : m_server(NULL) {}
-    virtual ~request_stream() {}
+	request_stream() : m_server(NULL) {}
+	virtual ~request_stream() {}
 
-    void set_server(const std::shared_ptr<Server> &server)
-    {
-        m_server = server;
-    }
+	void set_server(const std::shared_ptr<Server> &server)
+	{
+		m_server = server;
+	}
 
 protected:
-    std::shared_ptr<Server> get_server()
-    {
-        if (__builtin_expect(!m_server, false))
-            throw std::logic_error("request_stream::m_server must be initialized");
-        return m_server;
-    }
+	std::shared_ptr<Server> get_server()
+	{
+		if (__builtin_expect(!m_server, false))
+			throw std::logic_error("request_stream::m_server must be initialized");
+		return m_server;
+	}
 
 private:
-    std::shared_ptr<Server> m_server;
+	std::shared_ptr<Server> m_server;
 };
 
 template <typename Server>
 class simple_request_stream : public request_stream<Server>
 {
 public:
-    virtual void on_request(const swarm::network_request &req, const boost::asio::const_buffer &buffer) = 0;
+	virtual void on_request(const swarm::network_request &req, const boost::asio::const_buffer &buffer) = 0;
+
+protected:
+	const swarm::network_request &get_request()
+	{
+		return m_request;
+	}
 
 private:
-    void on_headers(const swarm::network_request &req)
-    {
-        m_request = req;
-        m_content_length = req.get_content_length();
+	void on_headers(const swarm::network_request &req)
+	{
+		m_request = req;
+		m_content_length = req.get_content_length();
 
-        if (m_content_length == 0) {
-            on_request(m_request, boost::asio::buffer("", 0));
-        } else {
-            m_data.reserve(m_content_length);
-        }
-    }
+		if (m_content_length == 0) {
+			on_request(m_request, boost::asio::buffer("", 0));
+		} else {
+			m_data.reserve(m_content_length);
+		}
+	}
 
-    void on_data(const boost::asio::const_buffer &buffer)
-    {
-        auto begin = boost::asio::buffer_cast<const char *>(buffer);
-        auto size = boost::asio::buffer_size(buffer);
-        m_data.insert(m_data.end(), begin, begin + size);
+	void on_data(const boost::asio::const_buffer &buffer)
+	{
+		auto begin = boost::asio::buffer_cast<const char *>(buffer);
+		auto size = boost::asio::buffer_size(buffer);
+		m_data.insert(m_data.end(), begin, begin + size);
 
-        if (m_data.size() == m_content_length) {
-            on_request(m_request, boost::asio::buffer(m_data.c_str(), m_data.size()));
-        } else if (m_data.size() > m_content_length) {
-            this->get_reply()->send_error(swarm::network_reply::bad_request);
-        }
-    }
+		if (m_data.size() == m_content_length) {
+			on_request(m_request, boost::asio::buffer(m_data.c_str(), m_data.size()));
+		} else if (m_data.size() > m_content_length) {
+			this->get_reply()->send_error(swarm::network_reply::bad_request);
+		}
+	}
 
-    swarm::network_request m_request;
-    std::string m_data;
-    size_t m_content_length;
+	swarm::network_request m_request;
+	std::string m_data;
+	size_t m_content_length;
 };
 
 }} // namespace ioremap::thevoid
