c516a228d86c0b14ce172f3d5a5d2f56372028e2
Ruslan Nigmatullin <euroelessar@yandex.ru>
2013-07-02 15:45:35 +0400

    Removed looking for cocaine

--- cmake/Modules/Findelliptics.cmake
+++ cmake/Modules/Findelliptics.cmake
@@ -1,12 +1,10 @@
 find_path(elliptics_INCLUDE_DIR elliptics/cppdef.h PATHS ${ELLIPTICS_PREFIX}/include /usr/include)
-find_path(cocaine_INCLUDE_DIR cocaine/common.hpp PATHS ${COCAINE_PREFIX}/include /usr/include)
 
-#find_library(elliptics_LIBRARY NAMES elliptics PATHS ${ELLIPTICS_PREFIX}/lib ${ELLIPTICS_PREFIX}/lib64 /usr/lib /usr/lib64)
 find_library(elliptics_cpp_LIBRARY NAMES elliptics_cpp PATHS ${ELLIPTICS_PREFIX}/lib ${ELLIPTICS_PREFIX}/lib64 /usr/lib /usr/lib64)
 find_library(elliptics_client_LIBRARY NAMES elliptics_client PATHS ${ELLIPTICS_PREFIX}/lib ${ELLIPTICS_PREFIX}/lib64 /usr/lib /usr/lib64)
 
-set(elliptics_LIBRARIES ${elliptics_LIBRARY} ${elliptics_cpp_LIBRARY} ${elliptics_client_LIBRARY})
-set(elliptics_INCLUDE_DIRS ${elliptics_INCLUDE_DIR} ${cocaine_INCLUDE_DIR})
+set(elliptics_LIBRARIES ${elliptics_cpp_LIBRARY} ${elliptics_client_LIBRARY})
+set(elliptics_INCLUDE_DIRS ${elliptics_INCLUDE_DIR})
 list(REMOVE_DUPLICATES elliptics_INCLUDE_DIRS)
 
 message(STATUS "elliptics_INCLUDE_DIR: " ${elliptics_INCLUDE_DIR})
