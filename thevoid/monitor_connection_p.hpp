23252f501a2f684e258ca18e923e59a1b0b5248c
Ruslan Nigmatullin <euroelessar@yandex.ru>
2014-07-31 16:52:10 +0400

    thevoid: Fixed compilation with gcc-4.4

--- thevoid/monitor_connection_p.hpp
+++ thevoid/monitor_connection_p.hpp
@@ -28,7 +28,7 @@ class monitor_connection : public std::enable_shared_from_this<monitor_connectio
 {
 public:
 	typedef boost::asio::ip::tcp::socket socket_type;
-	typedef typename socket_type::endpoint_type endpoint_type;
+	typedef socket_type::endpoint_type endpoint_type;
 
 	monitor_connection(boost::asio::io_service &io_service, size_t buffer_size);
 	~monitor_connection();
