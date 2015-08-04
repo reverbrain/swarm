c0d95329605b73de07c611281f5db5b7a3c4d2de
Danil Osherov <shindo@yandex-team.ru>
2015-08-04 14:03:53 +0300

    thevoid/acceptorlist: add actual endpoint to local_endpoints

--- thevoid/acceptorlist.cpp
+++ thevoid/acceptorlist.cpp
@@ -73,10 +73,12 @@ void acceptors_list<Connection>::add_acceptor(const std::string &address)
 		acceptor->bind(endpoint);
 		acceptor->listen(data.backlog_size);
 
-		BH_LOG(data.logger, SWARM_LOG_INFO, "Started to listen address: %s, backlog: %d",
-				boost::lexical_cast<std::string>(acceptor->local_endpoint()), data.backlog_size);
+		auto local_endpoint = boost::lexical_cast<std::string>(acceptor->local_endpoint());
 
-		local_endpoints.emplace_back(boost::lexical_cast<std::string>(endpoint));
+		BH_LOG(data.logger, SWARM_LOG_INFO, "Started to listen address: %s, backlog: %d",
+				local_endpoint, data.backlog_size);
+
+		local_endpoints.emplace_back(local_endpoint);
 		protocols.push_back(endpoint.protocol());
 
 		complete_socket_creation(endpoint);
